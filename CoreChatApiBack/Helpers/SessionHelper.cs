4ae141b6578795fa88f86fc27fd55c0f074af973
hossainalazad90 <hossainalazad@gmail.com>
2021-01-28 18:59:44 +0600

    change

---
+++ CoreChatApiBack/Helpers/SessionHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreChatApiBack.Helpers
+{
+    public static class SessionHelper
+    {
+        private const string userId = "UserId";
+        private const string fullName = "FullName";
+        private const string email = "Email";
+        private const string loginTime = "LoginTime";
+
+        public static string UserId
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session[userId];
+            }
+            set
+            {
+                HttpContext.Current.Session[userId] = value;
+            }
+        }
+
+        public static void Clear()
+        {
+            
+        }
+
+
+
+
+
+    }
+}
